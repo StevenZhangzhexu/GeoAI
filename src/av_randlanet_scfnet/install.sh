ff3d06a39336e40d17c8a91c90575b5753ec1f25
Matiur Rahman Minar <matiur.minar@net-targets.com>
2023-11-07 10:00:24 +0600

    flask predict and run.

--- src/av_randlanet_scfnet/install.sh
+++ src/av_randlanet_scfnet/install.sh
@@ -1,13 +1,12 @@
-# randlanet
+# randlanet installation
+# manually install the below commented stuff first
+# nvidia cuda driver and toolkit
 # conda create -n randlanet python=3.6
 # conda activate randlanet
 
-pip install -r helper_requirements.txt
+pip install -r requirements.txt
 sh compile_op.sh
-pip install tensorflow-gpu==1.*
-pip install laspy[lazrs,laszip]
 
 # check gpu with tf: python -c "import tensorflow as tf; print(tf.config.list_physical_devices('GPU'))"
 # check gpu with tf: python -c "import tensorflow as tf; print(tf.__version__)"
-# pip install open3d
 # pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu117
--- src/av_randlanet_scfnet/install_latest.sh
+++
@@ -1,8 +0,0 @@
-# randlanet
-conda create -n orchard python=3.6
-conda activate orchard
-
-pip install -r helper_requirements.txt
-sh compile_op.sh
-pip install tensorflow==2.*
-pip install laspy[lazrs,laszip]
