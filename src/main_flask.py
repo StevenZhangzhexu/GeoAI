56e1b4e7b8487b796e5f3b7fc5e04f9903a73966
Matiur Rahman Minar <matiur.minar@net-targets.com>
2023-11-20 08:37:48 +0600

    flask app separate objects with samlidar.

--- src/main_flask.py
+++ src/main_flask.py
@@ -43,8 +43,8 @@ def result():
 
             # separate_predicted_objects.separate_and_cluster_point_cloud_objects(f.filename)
             # sam_instance_segmentation.run_sam_instance_segmentation(f.filename)
-            # subprocess.run(['/home/pc1/miniconda3/envs/samlidar/bin/python', 'av_randlanet_scfnet/utils/sam_instance_segmentation.py', f.filename])
-            subprocess.run(['conda', 'run', '-n', 'samlidar', 'av_randlanet_scfnet/utils/sam_instance_segmentation.py', f.filename])
+            subprocess.run(['/home/pc1/miniconda3/envs/samlidar/bin/python', 'av_randlanet_scfnet/utils/sam_instance_segmentation.py', f.filename])
+            # subprocess.run(['conda', 'run', '-n', 'samlidar', 'av_randlanet_scfnet/utils/sam_instance_segmentation.py', f.filename])
             separate_predicted_objects.separate_and_cluster_point_cloud_objects(f.filename)
 
             # copy the results to shared folder
