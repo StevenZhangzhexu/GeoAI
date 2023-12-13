6b2ca18cdcad0b3397e75af871566b2537683dde
Matiur Rahman Minar <matiur.minar@net-targets.com>
2023-12-13 10:57:13 +0600

    open3d.

---
+++ src/av_randlanet_scfnet/install_sub.sh
@@ -0,0 +1,9 @@
+# randlanet installation
+# manually install the below commented stuff first
+# nvidia cuda driver and toolkit
+
+conda create -n samlidar -y python=3.11
+conda activate samlidar
+
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu117
+pip install open3d
