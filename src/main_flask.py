bce5ba0e0848e1dbd4d589cd5afc69634111dccc
Matiur Rahman Minar <matiur.minar@net-targets.com>
2023-12-15 07:55:56 +0600

    samlidar.

--- src/main_flask.py
+++ src/main_flask.py
@@ -3,7 +3,7 @@ from flask import *
 from av_randlanet_scfnet import predict_OrchardRoad
 from av_randlanet_scfnet.utils import data_prepare_orchard, separate_predicted_objects, helper_las
 from av_randlanet_scfnet.utils import sam_instance_segmentation
-from av_randlanet_scfnet import vis_pred_OrchardRoad
+# from av_randlanet_scfnet import vis_pred_OrchardRoad
 import subprocess
 
 
