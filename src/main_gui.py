d78f7d11ae346e1df654a8b565c4e86748991346
Matiur Rahman Minar <matiur.minar@net-targets.com>
2024-03-13 16:40:40 +0600

    install.

--- src/main_gui.py
+++ src/main_gui.py
@@ -1,3 +1,4 @@
+import os
 import kivy
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
@@ -6,10 +7,58 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.label import Label
 from kivy.properties import StringProperty, ObjectProperty
 from kivy.uix.filechooser import FileChooserIconView
+from kivy.uix.popup import Popup
+from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+from kivy.factory import Factory
 
 # Replace with your actual deep learning model and functions
-from av_randlanet_scfnet import predict_OrchardRoad
-from av_randlanet_scfnet.utils import data_prepare_orchard, separate_predicted_objects, helper_las
+# from av_randlanet_scfnet import predict_OrchardRoad
+# from av_randlanet_scfnet.utils import data_prepare_orchard, separate_predicted_objects, helper_las
+
+
+class LoadDialog(FloatLayout):
+    load = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+
+
+class SaveDialog(FloatLayout):
+    save = ObjectProperty(None)
+    text_input = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+
+
+class Root(FloatLayout):
+    loadfile = ObjectProperty(None)
+    savefile = ObjectProperty(None)
+    text_input = ObjectProperty(None)
+
+    def dismiss_popup(self):
+        self._popup.dismiss()
+
+    def show_load(self):
+        content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Load file", content=content,
+                            size_hint=(0.9, 0.9))
+        self._popup.open()
+
+    def show_save(self):
+        content = SaveDialog(save=self.save, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Save file", content=content,
+                            size_hint=(0.9, 0.9))
+        self._popup.open()
+
+    def load(self, path, filename):
+        with open(os.path.join(path, filename[0])) as stream:
+            self.text_input.text = stream.read()
+
+        self.dismiss_popup()
+
+    def save(self, path, filename):
+        with open(os.path.join(path, filename), 'w') as stream:
+            stream.write(self.text_input.text)
+
+        self.dismiss_popup()
 
 
 class PointCloudSegmentationApp(App):
@@ -51,7 +100,7 @@ class PointCloudSegmentationApp(App):
     def open_file_chooser(self, instance):
         file_chooser = FileChooserIconView()
         file_chooser.bind(on_selection=self.set_file_path)  # Bind on_selection event
-        file_chooser.open()
+        # file_chooser.open()
 
     def set_file_path(self, instance, value):
         if value:
