0d282b3273d63096b89f754833ea97012044698b
Steven Zhang <68343996+StevenZhangzhexu@users.noreply.github.com>
2024-03-14 18:02:46 +0800

    Add files via upload

--- src/main_flask1.py
+++ src/main_flask1.py
@@ -4,6 +4,8 @@ from av_randlanet_scfnet import predict_OrchardRoad
 from UNext import main_Unext_infer
 from av_randlanet_scfnet.utils import data_prepare_orchard, separate_predicted_objects, helper_las
 import subprocess
+from convert_shp import convert
+import pickle
 
 app = Flask(__name__, static_folder='static/')
 
@@ -16,44 +18,54 @@ def result():
     if request.method == 'POST':
         f = request.files['file']
         try:
-            # Upload file
-            upload_path = 'UNext/test_inputs'
-            file_path = os.path.join(upload_path, f.filename)
-            os.makedirs(upload_path, exist_ok=True)
-            f.save(file_path)
-
-            # Pre-process
-            data_prepare_orchard.prepare_data(file_path)
-
             # Determine which model to use
             option = request.form.get('option')  
             print('option,', option)
 
             # Predict
             if option == 'model0':
+                # upload file
+                upload_path = 'av_randlanet_scfnet/data/orchard_road/test_inputs'
+                file_path = os.path.join(upload_path, f.filename)
+                os.makedirs(upload_path, exist_ok=True)
+                f.save(file_path)
+                # pre-process
+                data_prepare_orchard.prepare_data(file_path)
                 predict_OrchardRoad.predict(filepath=file_path)
-            elif option == 'model1':
-                main_Unext_infer.predict(filepath=file_path, id=1)
-            elif option == 'model2':
-                main_Unext_infer.predict(filepath=file_path, id=2)
-            elif option == 'model3':
-                main_Unext_infer.predict(filepath=file_path, id=3)
-            elif option == 'model4':
-                main_Unext_infer.predict(filepath=file_path, id=4)
+            else:
+                # Upload file
+                upload_path = f'UNext/test_inputs/{f.filename[:-4]}'
+                file_path = os.path.join(upload_path, f.filename)
+                os.makedirs(upload_path, exist_ok=True)
+                f.save(file_path)
+                # Pre-process
+                data_prepare_orchard.prepare_data(pc_path = file_path,  dataset_path= upload_path)
+
+                if option == 'model1':
+                    chosen_folder = main_Unext_infer.predict(filepath=file_path, uploadpath = upload_path, id=1)
+                elif option == 'model2':
+                    chosen_folder = main_Unext_infer.predict(filepath=file_path,uploadpath = upload_path, id=2)
+                elif option == 'model3':
+                    chosen_folder = main_Unext_infer.predict(filepath=file_path,uploadpath = upload_path, id=3)
+                elif option == 'model4':
+                    chosen_folder = main_Unext_infer.predict(filepath=file_path,uploadpath = upload_path, id=4)
 
             # Post-process
             # separate_predicted_objects.separate_and_segment_point_clouds(f.filename)
             # helper_las.copy_predictions()
 
             # Run other scripts using subprocess if needed
-            # subprocess.run(['/home/steven/miniconda3/envs/samlidar/bin/python', 
-            #                 'av_randlanet_scfnet/utils/sam_instance_segmentation.py', f.filename])
             if option != 'model0':
                 subprocess.run(['/home/steven/miniconda3/envs/vis/bin/python', 
-                                'UNext/vis_pred.py', f.filename])
-            # subprocess.run(['/home/steven/miniconda3/envs/vis/bin/python', 
-            #                 'av_randlanet_scfnet/vis_pred_bboxes_OrchardRoad.py', f.filename])
-
+                                'UNext/vis_pred.py', f.filename])  
+            # chosen_folder = 'UNext/results/Pred_2024-03-14_08-34-07'
+            pkl_path = os.path.join(chosen_folder, 'bbox_dict.pkl')
+            with open(pkl_path, 'rb') as file:
+                bbox_dict = pickle.load(file)
+            shp_folder = chosen_folder +'/shp'   
+            if 'convertButton' in request.form:
+                convert(bbox_dict, folder=shp_folder)
+                
             print("All finished!")
             return render_template("success.html", name=f.filename)
         except Exception as err:
