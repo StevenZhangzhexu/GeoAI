440304ca8ed6366f20b289f5adaa55566c19cc06
Matiur Rahman Minar <matiur.minar@net-targets.com>
2024-03-22 09:41:05 +0600

    linux run.

--- install.sh
+++ install.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-conda create -n geoai -y python=3.6
-conda init
+# conda create -n geoai -y python=3.6
 conda activate geoai
 
 pip install -r requirements.txt
 sudo snap install ngrok
 
 # randlanet
-pip install -r av_randlanet_scfnet/requirements_main.txt
-sh av_randlanet_scfnet/compile_op.sh
+pip install -r src/av_randlanet_scfnet/requirements_main.txt
+sh src/av_randlanet_scfnet/compile_op.sh
 
 $SHELL
