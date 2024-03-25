6cf91f6574cac8219cfce6934a779ad5ecc2efcc
Matiur Rahman Minar <matiur.minar@net-targets.com>
2024-03-25 14:03:37 +0600

    demo page designs.

--- src/main_flask.py
+++ src/main_flask.py
@@ -69,10 +69,10 @@ def result():
 
             # visualize predictions
             # vis_pred_OrchardRoad.viz_pred(f.filename)
-            subprocess.run(['/home/pc1/miniconda3/envs/open3d/bin/python',
-                            'av_randlanet_scfnet/vis_pred_semseg_OrchardRoad.py', f.filename])
-            subprocess.run(['/home/pc1/miniconda3/envs/open3d/bin/python',
-                            'av_randlanet_scfnet/vis_pred_bboxes_OrchardRoad.py', f.filename])
+            # subprocess.run(['/home/pc1/miniconda3/envs/open3d/bin/python',
+            #                 'av_randlanet_scfnet/vis_pred_semseg_OrchardRoad.py', f.filename])
+            # subprocess.run(['/home/pc1/miniconda3/envs/open3d/bin/python',
+            #                 'av_randlanet_scfnet/vis_pred_bboxes_OrchardRoad.py', f.filename])
 
             print("All finished!")
 
