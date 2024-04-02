9772c3880845cde84decee94de21004bc838d17b
Matiur Rahman Minar <matiur.minar@net-targets.com>
2024-04-02 16:04:47 +0600

    labels save.

--- src/tools/shapefile_conversion_ver3.py
+++ src/tools/shapefile_conversion_ver3.py
@@ -84,13 +84,10 @@ def get_center_base_coords(coordinates, label_id):
         center_coordinate = np.mean(coordinates, axis=0)
     print(center_coordinate)
 
-    center = {
-        'x': center_coordinate[0],
-        'y': center_coordinate[1],
-        'z': base_point[2],
-    }
+    base_center = center_coordinate.copy()
+    base_center[2] = base_point[2]
 
-    return center
+    return base_center
 
 
 def bbox_pcd(pc_path, visualize=False, visualize_by_cat=False):
